4ed100f4c677ca0f5a21817738d882c7ec848d28
AbiMeGab <abbybaem@gmail.com>
2022-09-06 11:48:17 -0500

    chore | ya usé una función

--- main/main.js
+++ main/main.js
@@ -5,3 +5,19 @@
 let lista = [3, 5, 7, 1, 6];
 console.log(Math.max(3, 5, 7, 1, 6));
 console.log(Math.max(...lista));
+
+
+function MyMax(nums) {
+    var n = nums.length;
+    maximo = nums[n-1];
+    while (n--){
+        if(nums[n] > maximo) {
+            maximo = nums[n]
+        }
+    }
+    return maximo;
+};
+
+var nums = [3, 5, 7, 1, 6];
+var m = MyMax(nums);
+console.log(m)
