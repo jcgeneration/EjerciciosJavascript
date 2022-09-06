420abc1eff3ee2a16bcb678092375b587da46217
AbiMeGab <abbybaem@gmail.com>
2022-09-06 11:08:19 -0500

    Primer ejercicio resuelto

---
+++ main/main1.js
@@ -0,0 +1,7 @@
+// ## Ejercicio #1
+// Escribir una función que tome un arreglo de números y devuelva el número mayor de la lista. 
+// Ejemplo del arreglo : [3, 5, 7, 1, 6]
+
+let lista = [3, 5, 7, 1, 6];
+console.log(Math.max(3, 5, 7, 1, 6));
+console.log(Math.max(...lista));
