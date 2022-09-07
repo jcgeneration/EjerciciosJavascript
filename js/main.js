96e79037e2c55ad104ec612237aadce4db27b81c
Akari <ada_hack313@hackademy.mx>
2022-09-07 15:13:34 -0500

    Ejercicio 2

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ let res = document.getElementById("resultado");
             res.innerHTML = "El número mayor es " + array[2];
 
  });
+
+ let res2 = document.getElementById("resultado");
+
+        let btn2 = document.getElementById("btnValidar2");
+
+        btn.addEventListener("click", function(e){
+            e.preventDefault();
+            console.log("btn click");
+            let a1 = parseInt(document.getElementById("nu1").value);
+            let b1 = parseInt(document.getElementById("nu2").value);
+            let c1 = parseInt(document.getElementById("nu3").value);
+
+            let array = [a2,b2,c2];
+
+            array.sort();
+            
+            res2.innerHTML = "El número menor es " + array[0];
+
+ });
